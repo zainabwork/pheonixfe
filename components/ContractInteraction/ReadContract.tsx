a6ff2d0598b7651e14b3152a7311a14999a1fe31
zainab rasheed <zainab-rasheed@netixsol.com>
2024-05-13 18:48:44 +0500

    wei to eth cnverter

--- components/ContractInteraction/ReadContract.tsx
+++ components/ContractInteraction/ReadContract.tsx
@@ -1,24 +1,37 @@
 "use client";
 
 import { useReadContract } from "wagmi";
-import { ethers } from "ethers";
+// import * as utils from 'bignumber.js';
+const convert = require('ethereum-unit-converter')
 
 export default function ReadContract() {
     const abi = [{"inputs":[{"internalType":"string","name":"name_","type":"string"},{"internalType":"string","name":"symbol_","type":"string"},{"internalType":"uint8","name":"decimals_","type":"uint8"},{"internalType":"uint256","name":"initialBalance_","type":"uint256"},{"internalType":"bytes","name":"signature_","type":"bytes"},{"internalType":"address payable","name":"feeReceiver_","type":"address"}],"stateMutability":"payable","type":"constructor"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"allowance","type":"uint256"},{"internalType":"uint256","name":"needed","type":"uint256"}],"name":"ERC20InsufficientAllowance","type":"error"},{"inputs":[{"internalType":"address","name":"sender","type":"address"},{"internalType":"uint256","name":"balance","type":"uint256"},{"internalType":"uint256","name":"needed","type":"uint256"}],"name":"ERC20InsufficientBalance","type":"error"},{"inputs":[{"internalType":"address","name":"approver","type":"address"}],"name":"ERC20InvalidApprover","type":"error"},{"inputs":[{"internalType":"address","name":"receiver","type":"address"}],"name":"ERC20InvalidReceiver","type":"error"},{"inputs":[{"internalType":"address","name":"sender","type":"address"}],"name":"ERC20InvalidSender","type":"error"},{"inputs":[{"internalType":"address","name":"spender","type":"address"}],"name":"ERC20InvalidSpender","type":"error"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"}]
+    const address = "0x5A6034D4B34FbaA937F1E5d4A8Ea8820A683D2e9";
 
-  const { data: balance, error } = useReadContract({
+  const { data:balance, error:supplyError } = useReadContract({
     abi,
-    address: "0x5A6034D4B34FbaA937F1E5d4A8Ea8820A683D2e9",
+    address,
     functionName: "totalSupply",
-    // args: ['0xfBad43ce91d5968E272EF0dc159bDdC7F9e3Db9d'],
+    // args: ['0x6bE76e3D11F388720884dfBA16e5dE31475F8B25'],
   });
-  if (error) {
-    console.error("Error fetching balance:", error);
-    return <div>Error fetching</div>;
-  }
-  
-  //   const weivalue= balance;
-  // const ethValue = ethers.formatEther(weivalue);
+  const { data: symbolData, error:symbolError } = useReadContract({
+    abi,
+    address,
+    functionName: "symbol",
+  });
 
-  return <div>Balance: {balance?.toString()}</div>;
+  if (supplyError) {
+    console.error("Error fetching balance:", supplyError);
+  }
+  if (symbolError) {
+    console.error("Error fetching balance:", symbolError);
+  }
+
+  return (
+    <>
+    {/* <div>Balance: {balance?.toString()}</div>; */}
+    <div>BAlance: {balance? convert(balance, 'wei', 'ether') : "Not Fetched Yet"} {symbolData}</div>
+    
+    </>
+  )
 }
